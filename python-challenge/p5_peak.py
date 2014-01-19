546dd3d44dd0222ee8d25ccf48adbdde6d53957f
julenka <julenka@cs.cmu.edu>
2014-01-19 11:45:10 -0500

    python challenge p5 pickle solution

--- python-challenge/p5_peak.py
+++ python-challenge/p5_peak.py
@@ -1,1 +1,6 @@
 __author__ = 'julenka'
+
+from pickle import Unpickler
+unpickled = Unpickler(open('p5_data.p', 'r')).load()
+for line in unpickled:
+    print ''.join([v * cnt for (v,cnt) in line])
