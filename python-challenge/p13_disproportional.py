06e4ba1f1fcb0f54431b3e393b0cd0b20fbbfd27
julenka <julenka@cs.cmu.edu>
2014-02-07 09:23:17 -0500

    Solving python challenge p13

---
+++ python-challenge/p13_disproportional.py
@@ -0,0 +1,21 @@
+__author__ = 'julenka'
+
+
+import xmlrpclib
+
+# create a ServerProxy with an URI that doesn't respond to XMLRPC requests
+proxy = xmlrpclib.ServerProxy("http://huge:file@www.pythonchallenge.com/pc/phonebook.php")
+
+def phone(s):
+    try:
+        print "trying",s
+        # I need to figure out which number to call
+        resp = proxy.phone(s)
+
+        return resp
+    except xmlrpclib.Fault as err:
+        print "A fault occurred"
+        print "Fault code: %d" % err.faultCode
+        print "Fault string: %s" % err.faultString
+
+print phone("Bert")
