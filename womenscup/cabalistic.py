7b7754db5bf2348dc4e1c0092e579d2ac17eed58
Julia Schwarz <julenka@cs.cmu.edu>
2015-10-10 18:18:45 -0700

    cabalistic

--- womenscup/cabalistic.py
+++ womenscup/cabalistic.py
@@ -5,6 +5,10 @@
 __author__ = 'julenka'
 from collections import defaultdict
 from collections import deque
+
+max_color = 0
+num_colors = 0
+
 def read_tuple_int():
     s = raw_input()
     return [int(x) for x in s.split()]
@@ -15,22 +19,43 @@ def fail():
 def success():
     print "Go on get the Magical Lamp"
 
+def merge_colors(npar, c1, c2):
+    global num_colors
+    npar[npar == c2] = c1
+    num_colors -= 1
+
+
+def get_color(npar, n):
+    global max_color, num_colors
+    c = npar[n]
+    if c == 0:
+        max_color += 1
+        c = max_color
+        npar[n] = c
+        num_colors += 1
+    return c
+
 import numpy as np
 def process_case():
     N, M = read_tuple_int()
     outgoing_edges = defaultdict(list)
     out_counts = defaultdict(int)
     in_counts = defaultdict(int)
-    dirty_bits_2 = np.zeros(N+1)
-    dirty_bits = np.zeros(N+1)
+    colors = np.zeros(N)
 
     for _ in range(M):
         f, t = [int(x) for x in raw_input().split()]
         outgoing_edges[f].append(t)
         out_counts[f] += 1
         in_counts[t] += 1
-        dirty_bits_2[f] = 1
-        dirty_bits_2[f] = 1
+        f_color = get_color(colors, f - 1)
+        t_color = get_color(colors, t - 1)
+        if f_color != t_color:
+            merge_colors(colors, f_color, t_color)
+
+    if num_colors != 1:
+        print num_colors, colors
+        return fail()
 
     if out_counts[N] == 0:
         fail()
@@ -44,28 +69,14 @@ def process_case():
             fail()
             return
 
-    q = deque([1])
 
-    while len(q) > 0:
-        cur = q.popleft()
-        dirty_bits[cur] = 1
-        nodes_to_visit = outgoing_edges[cur]
-        for node in nodes_to_visit:
-            if dirty_bits[node] == 1:
-                continue
-            q.append(node)
-
-    if dirty_bits[N] == 0:
-        fail()
-        return
-
-    if dirty_bits.sum() != dirty_bits_2.sum():
-        fail()
-        return
-    success()
+    return success()
 
 
 if __name__ == '__main__':
+    global num_colors, max_color
     T = input()
     for i in range(T):
+        max_color = 0
+        num_colors = 0
         process_case()
